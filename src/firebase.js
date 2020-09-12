d4493f00cdfb465f2008f5ab7795345ec7843719
Moazam05 <salmanmoazam08@gmail.com>
2020-09-12 15:44:03 +0500

    Add Firebase Code Remove Default CSS

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyB63VjaZLZvigk8n4mtL1iVhgEaVnAHYlM',
+  authDomain: 'clone-react-js.firebaseapp.com',
+  databaseURL: 'https://clone-react-js.firebaseio.com',
+  projectId: 'clone-react-js',
+  storageBucket: 'clone-react-js.appspot.com',
+  messagingSenderId: '10949720307',
+  appId: '1:10949720307:web:cd11dc85ab2f167f079f86',
+  measurementId: 'G-S2FZLBN5MS',
+};
