c3ff1c48b8168c793839dee1d6e6985654fc5956
Moazam05 <salmanmoazam08@gmail.com>
2020-09-14 02:16:26 +0500

    Done Home Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,16 +14,52 @@ function Home() {
             alt='Home Logo'
           />
           <div className='home_row'>
-            <Product />
-            <Product />
+            <Product
+              id='1'
+              title='Learning React: Modern Patterns for Developing React Apps'
+              price={39.21}
+              image='https://images-na.ssl-images-amazon.com/images/I/51Kwaw5nInL._SX379_BO1,204,203,200_.jpg'
+              rating={5}
+            />
+            <Product
+              id='2'
+              title='ASUS VivoBook 15 Thin and Light Laptop, 15.6” FHD, Intel i5-1035G1 CPU, 8GB RAM, 512GB SSD, Backlit KB, Fingerprint, Windows 10, Slate Gray, F512JA-AS54'
+              price={599.99}
+              image='https://images-na.ssl-images-amazon.com/images/I/81fstJkUlaL._AC_SL1500_.jpg'
+              rating={4}
+            />
           </div>
           <div className='home_row'>
-            <Product />
-            <Product />
-            <Product />
+            <Product
+              id='3'
+              title='AmazonBasics 15.6" Laptop Bag Professional - Black'
+              price={17.99}
+              image='https://images-na.ssl-images-amazon.com/images/I/81R5dG1XyLL._AC_SL1500_.jpg'
+              rating={3}
+            />
+            <Product
+              id='4'
+              title='Samsung Galaxy A51 Factory Unlocked Cell Phone | 128GB of Storage | Long Lasting Battery | Single SIM | GSM or CDMA Compatible | US Version | Black'
+              price={399.99}
+              image='https://images-na.ssl-images-amazon.com/images/I/71QZcCFQcAL._AC_SL1500_.jpg'
+              rating={5}
+            />
+            <Product
+              id='5'
+              title='IP68 Swimming Waterproof Smartwatch Fitness Tracker Fitness Watch Heart Rate Monitor Watches for Men Women'
+              price={178.95}
+              image='https://images-na.ssl-images-amazon.com/images/I/617aPQkNYHL._AC_SL1000_.jpg'
+              rating={4}
+            />
           </div>
           <div className='home_row'>
-            <Product />
+            <Product
+              id='5'
+              title='Samsung 49-Inch CRG9 Curved Gaming Monitor (LC49RG90SSNXZA) – 120Hz Refresh, Ultrawide Screen QLED Computer Monitor, 5120 x 1440p Resolution, 4ms Response, FreeSync 2 with HDR, HDMI'
+              price={129.99}
+              image='https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg'
+              rating={4}
+            />
           </div>
         </div>
       </div>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product({ title, image, rating, price }) {
   return (
     <>
       <div className='product'>
         <div className='product_info'>
-          <p>The lean startup</p>
+          <p>{title}</p>
           <p className='product_price'>
             <small>$</small>
-            <strong>19.96</strong>
+            <strong>{price}</strong>
           </p>
           <div className='product_rating'>
-            <p>🌟</p>
+            {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <p>🌟</p>
+              ))}
           </div>
         </div>
-        <img
-          src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
-          alt=''
-        />
+
+        <img src={image} alt='' />
         <button>Add to Basket</button>
       </div>
     </>
