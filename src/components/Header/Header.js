81674e9eaea5341095c8f2e975c5d7362a8ba946
Moazam05 <salmanmoazam08@gmail.com>
2020-09-14 14:58:30 +0500

    Create Context API & Passing Data

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { useStateValue } from '../ContextAPI/StateProvider';
 
 function Header() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <>
       <div className='header'>
-        <img
-          src='https://pngimg.com/uploads/amazon/amazon_PNG25.png'
-          alt='Company Logo'
-          className='header_logo'
-        />
+        <Link to='/'>
+          <img
+            src='https://pngimg.com/uploads/amazon/amazon_PNG25.png'
+            alt='Company Logo'
+            className='header_logo'
+          />
+        </Link>
 
         <div className='header_search'>
           <input className='header_searchInput' type='text' />
@@ -35,10 +41,14 @@ function Header() {
             <span className='header_optionOne'>Your</span>
             <span className='header_optionTwo'>Prime</span>
           </div>
-          <div className='header_optionBasket'>
-            <ShoppingBasketIcon />
-            <span className='header_optionTwo header_basketCount'>0</span>
-          </div>
+          <Link to='/checkout'>
+            <div className='header_optionBasket'>
+              <ShoppingBasketIcon />
+              <span className='header_optionTwo header_basketCount'>
+                {basket?.length}
+              </span>
+            </div>
+          </Link>
         </div>
       </div>
     </>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useStateValue } from '../ContextAPI/StateProvider';
 import './Product.css';
 
-function Product({ title, image, rating, price }) {
+function Product({ id, title, image, rating, price }) {
+  const [state, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    // ! dispatch the item into the data layer
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id,
+        title,
+        image,
+        rating,
+        price,
+      },
+    });
+  };
+
   return (
     <>
       <div className='product'>
@@ -21,7 +38,7 @@ function Product({ title, image, rating, price }) {
         </div>
 
         <img src={image} alt='' />
-        <button>Add to Basket</button>
+        <button onClick={addToBasket}>Add to Basket</button>
       </div>
     </>
   );
